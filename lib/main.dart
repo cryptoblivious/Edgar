f66c511fd802ff97746055335da502f90fc17c7e
cryptoblivious <andrzej.wis123@gmail.com>
2023-06-01 14:07:02 -0400

    Working on multiple pages

--- lib/main.dart
+++ lib/main.dart
@@ -10,28 +10,27 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Edgar : Your Personal Chef',
       theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
           fontFamily: 'Montez',
           useMaterial3: true,
           splashColor: Colors.amber[800]!.withAlpha(50)),
-      home: const PantryPage(titleTextValue: 'Edgar : Your Personal Chef'),
+      home: const Home(titleTextValue: 'Edgar : Your Personal Chef'),
     );
   }
 }
 
-class PantryPage extends StatefulWidget {
-  const PantryPage({super.key, required this.titleTextValue});
+class Home extends StatefulWidget {
+  const Home({super.key, required this.titleTextValue});
 
   final String titleTextValue;
 
   @override
-  State<PantryPage> createState() => _PantryPageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _PantryPageState extends State<PantryPage> {
-  int _counter = 0;
+class _HomeState extends State<Home> {
   int _selectedIndex = 0;
 
   void _onItemTapped(int index) {
@@ -41,14 +40,22 @@ class _PantryPageState extends State<PantryPage> {
     });
   }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    Widget body;
+    switch (_selectedIndex) {
+      case 0:
+        body = const PantryBody();
+        break;
+      case 1:
+        body = const Text('Recipes');
+        break;
+      case 2:
+        body = const Text('Groceries');
+        break;
+      default:
+        body = const Text('Pantry');
+    }
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.black,
@@ -84,41 +91,7 @@ class _PantryPageState extends State<PantryPage> {
           ),
           centerTitle: true,
         ),
-        body: Container(
-          color: Colors.grey[800],
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ProductIconLabelButton(
-                        productName: 'Potato',
-                        productIcon: Icons.lunch_dining,
-                        onPressed: _incrementCounter),
-                    const ProductIconLabelButton(
-                        productName: 'Tomato',
-                        productIcon: Icons.dinner_dining),
-                    const ProductIconLabelButton(
-                        productName: 'Mango', productIcon: Icons.brunch_dining),
-                  ],
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(5),
-                color: Theme.of(context).colorScheme.secondary,
-                child: Text(
-                  'You have pushed the potato button this many times : $_counter',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 32,
-                    color: Theme.of(context).colorScheme.onSecondary,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+        body: body,
         bottomNavigationBar: Container(
           decoration: BoxDecoration(
             color: Theme.of(context)
@@ -166,7 +139,7 @@ class ProductIconLabelButton extends StatelessWidget {
       {super.key,
       required this.productName,
       required this.productIcon,
-      this.onPressed});
+      required this.onPressed});
 
   final String productName;
   final IconData productIcon;
@@ -174,32 +147,135 @@ class ProductIconLabelButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton.icon(
-      onPressed: onPressed,
-      icon: Icon(
-        productIcon,
-      ),
-      label: Text(
-        productName,
-        style: const TextStyle(
-          fontSize: 25,
+    return Container(
+      margin: const EdgeInsets.all(5),
+      child: ElevatedButton.icon(
+        onPressed: onPressed,
+        icon: Icon(
+          productIcon,
+        ),
+        label: Text(
+          productName,
+          style: const TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all<Color>(
+            Theme.of(context).colorScheme.onPrimary,
+          ),
+          backgroundColor: MaterialStateProperty.all<Color>(
+            Theme.of(context).colorScheme.primary,
+          ),
+          overlayColor: MaterialStateProperty.resolveWith<Color?>(
+            (Set<MaterialState> states) {
+              if (states.contains(MaterialState.pressed)) {
+                return Colors.amber[800]!.withAlpha(50);
+              }
+              return null; // Defer to the widget's default.
+            },
+          ),
         ),
       ),
-      style: ButtonStyle(
-        foregroundColor: MaterialStateProperty.all<Color>(
-          Theme.of(context).colorScheme.onPrimary,
-        ),
-        backgroundColor: MaterialStateProperty.all<Color>(
-          Theme.of(context).colorScheme.primary,
-        ),
-        overlayColor: MaterialStateProperty.resolveWith<Color?>(
-          (Set<MaterialState> states) {
-            if (states.contains(MaterialState.pressed)) {
-              return Colors.amber[800]!.withAlpha(50);
-            }
-            return null; // Defer to the widget's default.
-          },
-        ),
+    );
+  }
+}
+
+class PantryBody extends StatefulWidget {
+  const PantryBody({super.key});
+
+  @override
+  State<PantryBody> createState() => _PantryBodyState();
+}
+
+class _PantryBodyState extends State<PantryBody> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.grey[800],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Center(
+            child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return Container(
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)),
+                  width: constraints.maxWidth * 0.5,
+                  child: const Text(
+                    'Search bar goes here',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 24, color: Colors.black),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: Wrap(
+              alignment: WrapAlignment.start,
+              children: [
+                ProductIconLabelButton(
+                    productName: 'Potato',
+                    productIcon: Icons.lunch_dining,
+                    onPressed: _incrementCounter),
+                ProductIconLabelButton(
+                    productName: 'Tomato',
+                    productIcon: Icons.dinner_dining,
+                    onPressed: () {}),
+                ProductIconLabelButton(
+                    productName: 'Mango',
+                    productIcon: Icons.brunch_dining,
+                    onPressed: () {}),
+                ProductIconLabelButton(
+                    productName: 'Banana',
+                    productIcon: Icons.breakfast_dining,
+                    onPressed: () {}),
+                ProductIconLabelButton(
+                    productName: 'Apple',
+                    productIcon: Icons.food_bank,
+                    onPressed: () {}),
+                ProductIconLabelButton(
+                    productName: 'Orange',
+                    productIcon: Icons.food_bank_outlined,
+                    onPressed: () {}),
+                ProductIconLabelButton(
+                    productName: 'Pineapple',
+                    productIcon: Icons.food_bank_rounded,
+                    onPressed: () {}),
+                ProductIconLabelButton(
+                    productName: 'Grapes',
+                    productIcon: Icons.food_bank_sharp,
+                    onPressed: () {}),
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(5),
+            color: Theme.of(context).colorScheme.secondary,
+            child: Text(
+              'You have pushed the potato button this many times : $_counter',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 32,
+                color: Theme.of(context).colorScheme.onSecondary,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
