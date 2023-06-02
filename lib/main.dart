f0ff8ba109982d4a88ce3249c6471eab455d4416
cryptoblivious <andrzej.wis123@gmail.com>
2023-06-01 23:03:40 -0400

    Search submenu going on well and multiple submenu placeholders added

--- lib/main.dart
+++ lib/main.dart
@@ -16,45 +16,60 @@ class App extends StatelessWidget {
           fontFamily: 'Montez',
           useMaterial3: true,
           splashColor: Colors.amber[800]!.withAlpha(50)),
-      home: const Home(titleTextValue: 'Edgar : Your Personal Chef'),
+      home: const Home(),
     );
   }
 }
 
 class Home extends StatefulWidget {
-  const Home({super.key, required this.titleTextValue});
-
-  final String titleTextValue;
+  const Home({super.key});
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  int _selectedIndex = 0;
+  int _selectedMenuIndex = 0;
 
   void _onItemTapped(int index) {
     setState(() {
-      _selectedIndex = index;
-      print(_selectedIndex);
+      _selectedMenuIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     Widget body;
-    switch (_selectedIndex) {
+    switch (_selectedMenuIndex) {
       case 0:
-        body = const PantryBody();
+        body = const PantryPage();
         break;
       case 1:
-        body = const Text('Recipes');
+        body = Container(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: const Center(
+                child: Text('Recipes', style: TextStyle(fontSize: 64))));
         break;
       case 2:
-        body = const Text('Groceries');
+        body = Container(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: const Center(
+                child: Text('Groceries', style: TextStyle(fontSize: 64))));
+        break;
+      case 3:
+        body = Container(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: const Center(
+                child: Text('Profile', style: TextStyle(fontSize: 64))));
+        break;
+      case 4:
+        body = Container(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: const Center(
+                child: Text('Settings', style: TextStyle(fontSize: 64))));
         break;
       default:
-        body = const Text('Pantry');
+        throw UnimplementedError('No widget for index $_selectedMenuIndex');
     }
     return Scaffold(
         appBar: AppBar(
@@ -64,7 +79,7 @@ class _HomeState extends State<Home> {
           title: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(widget.titleTextValue,
+              Text('Edgar, your personal Chef',
                   style: TextStyle(
                     color: Theme.of(context).colorScheme.onPrimary,
                     fontWeight: FontWeight.bold,
@@ -92,73 +107,87 @@ class _HomeState extends State<Home> {
           centerTitle: true,
         ),
         body: body,
-        bottomNavigationBar: Container(
-          decoration: BoxDecoration(
-            color: Theme.of(context)
-                .colorScheme
-                .background, // Set the desired background color for the ledge
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.3),
-                blurRadius: 5,
-                spreadRadius: 2,
-              ),
-            ],
-          ),
-          child: BottomNavigationBar(
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
-            iconSize: 36,
-            unselectedItemColor: Theme.of(context).colorScheme.onPrimary,
-            selectedItemColor: Colors.amber[800],
-            unselectedFontSize: 18,
-            selectedFontSize: 24,
-            landscapeLayout: BottomNavigationBarLandscapeLayout.centered,
-            backgroundColor: Theme.of(context).colorScheme.primary,
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.view_in_ar_sharp),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedMenuIndex,
+          onTap: _onItemTapped,
+          iconSize: 24,
+          unselectedItemColor: Theme.of(context).colorScheme.onPrimary,
+          selectedItemColor: Colors.amber[800],
+          unselectedFontSize: 18,
+          selectedFontSize: 24,
+          landscapeLayout: BottomNavigationBarLandscapeLayout.centered,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.inventory_2_outlined),
                 label: 'Pantry',
-              ),
-              BottomNavigationBarItem(
+                backgroundColor: Colors.black),
+            BottomNavigationBarItem(
                 icon: Icon(Icons.dining_outlined),
                 label: 'Recipes',
-              ),
-              BottomNavigationBarItem(
+                backgroundColor: Colors.black),
+            BottomNavigationBarItem(
                 icon: Icon(Icons.shopping_basket_outlined),
                 label: 'Groceries',
-              ),
-            ],
-          ),
+                backgroundColor: Colors.black),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline),
+                label: 'Profile',
+                backgroundColor: Colors.black),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings_outlined),
+                label: 'Settings',
+                backgroundColor: Colors.black),
+          ],
         ));
   }
 }
 
-class ProductIconLabelButton extends StatelessWidget {
+class ProductIconLabelButton extends StatefulWidget {
   const ProductIconLabelButton(
-      {super.key,
-      required this.productName,
-      required this.productIcon,
-      required this.onPressed});
+      {super.key, required this.productName, required this.productIcon});
 
   final String productName;
   final IconData productIcon;
-  final VoidCallback? onPressed;
+
+  @override
+  State<ProductIconLabelButton> createState() => _ProductIconLabelButtonState();
+}
+
+class _ProductIconLabelButtonState extends State<ProductIconLabelButton> {
+  List<IconData> icons = [
+    Icons.add,
+    Icons.cached,
+    Icons.one_x_mobiledata,
+  ];
+
+  void _rotateIcon() {
+    setState(() {
+      //make it so the second icon is the first icon, and the first icon is the last icon
+      icons.add(icons.removeAt(0));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(5),
-      child: ElevatedButton.icon(
-        onPressed: onPressed,
+      child: TextButton.icon(
+        onPressed: _rotateIcon,
         icon: Icon(
-          productIcon,
+          widget.productIcon,
         ),
-        label: Text(
-          productName,
-          style: const TextStyle(
-            fontSize: 20,
-          ),
+        label: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              widget.productName,
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(width: 10),
+            Icon(icons[0]),
+          ],
         ),
         style: ButtonStyle(
           foregroundColor: MaterialStateProperty.all<Color>(
@@ -181,39 +210,37 @@ class ProductIconLabelButton extends StatelessWidget {
   }
 }
 
-class PantryBody extends StatefulWidget {
-  const PantryBody({super.key});
+class PantrySearchContent extends StatefulWidget {
+  const PantrySearchContent({super.key});
 
   @override
-  State<PantryBody> createState() => _PantryBodyState();
+  State<PantrySearchContent> createState() => _PantrySearchContentState();
 }
 
-class _PantryBodyState extends State<PantryBody> {
-  int _counter = 0;
+class _PantrySearchContentState extends State<PantrySearchContent> {
+  String? selectedItem;
 
-  void _incrementCounter() {
+  void _setSelectedItem(String e) {
     setState(() {
-      _counter++;
+      selectedItem = e;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      color: Colors.grey[800],
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Center(
-            child: LayoutBuilder(
+    return Expanded(
+      child: Container(
+        color: Colors.grey[800],
+        child: Column(
+          children: <Widget>[
+            LayoutBuilder(
               builder: (BuildContext context, BoxConstraints constraints) {
                 return Container(
                   margin: const EdgeInsets.all(10),
                   decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(10)),
-                  width: constraints.maxWidth * 0.5,
+                  width: constraints.maxWidth,
                   child: const Text(
                     'Search bar goes here',
                     textAlign: TextAlign.center,
@@ -222,60 +249,150 @@ class _PantryBodyState extends State<PantryBody> {
                 );
               },
             ),
-          ),
-          Expanded(
-            child: Wrap(
-              alignment: WrapAlignment.start,
-              children: [
-                ProductIconLabelButton(
+            const Expanded(
+              child: Wrap(
+                children: [
+                  ProductIconLabelButton(
                     productName: 'Potato',
                     productIcon: Icons.lunch_dining,
-                    onPressed: _incrementCounter),
-                ProductIconLabelButton(
+                  ),
+                  ProductIconLabelButton(
                     productName: 'Tomato',
                     productIcon: Icons.dinner_dining,
-                    onPressed: () {}),
-                ProductIconLabelButton(
+                  ),
+                  ProductIconLabelButton(
                     productName: 'Mango',
                     productIcon: Icons.brunch_dining,
-                    onPressed: () {}),
-                ProductIconLabelButton(
+                  ),
+                  ProductIconLabelButton(
                     productName: 'Banana',
                     productIcon: Icons.breakfast_dining,
-                    onPressed: () {}),
-                ProductIconLabelButton(
+                  ),
+                  ProductIconLabelButton(
                     productName: 'Apple',
                     productIcon: Icons.food_bank,
-                    onPressed: () {}),
-                ProductIconLabelButton(
+                  ),
+                  ProductIconLabelButton(
                     productName: 'Orange',
                     productIcon: Icons.food_bank_outlined,
-                    onPressed: () {}),
-                ProductIconLabelButton(
+                  ),
+                  ProductIconLabelButton(
                     productName: 'Pineapple',
                     productIcon: Icons.food_bank_rounded,
-                    onPressed: () {}),
-                ProductIconLabelButton(
+                  ),
+                  ProductIconLabelButton(
                     productName: 'Grapes',
                     productIcon: Icons.food_bank_sharp,
-                    onPressed: () {}),
-              ],
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(5),
-            color: Theme.of(context).colorScheme.secondary,
-            child: Text(
-              'You have pushed the potato button this many times : $_counter',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 32,
-                color: Theme.of(context).colorScheme.onSecondary,
+                  ),
+                ],
               ),
             ),
-          ),
-        ],
+            Container(
+              width: double.infinity,
+              color: Colors.white,
+              padding: const EdgeInsets.all(10),
+              child: const Text('No of search results goes here',
+                  textAlign: TextAlign.center, style: TextStyle(fontSize: 24)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PantryPage extends StatefulWidget {
+  const PantryPage({super.key});
+
+  @override
+  State<PantryPage> createState() => _PantryPageState();
+}
+
+class _PantryPageState extends State<PantryPage> {
+  int _selectedSubmenuIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget body;
+    switch (_selectedSubmenuIndex) {
+      case 0:
+        body = const PantrySearchContent();
+        break;
+      case 1:
+        body = Expanded(
+          child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: const Center(
+                  child: Text('Inventory', style: TextStyle(fontSize: 64)))),
+        );
+        break;
+      case 2:
+        body = Expanded(
+          child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: const Center(
+                  child: Text('Staples', style: TextStyle(fontSize: 64)))),
+        );
+        break;
+      case 3:
+        body = Expanded(
+          child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: const Center(
+                  child: Text('One timers', style: TextStyle(fontSize: 64)))),
+        );
+      default:
+        throw UnimplementedError('No widget for index $_selectedSubmenuIndex');
+    }
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) => Container(
+        width: double.infinity,
+        color: Colors.grey[800],
+        child: Row(
+          children: [
+            SafeArea(
+                child: NavigationRail(
+              backgroundColor: Theme.of(context).colorScheme.secondary,
+              selectedIndex: _selectedSubmenuIndex,
+              unselectedIconTheme: IconThemeData(
+                  color: Theme.of(context).colorScheme.onSecondary),
+              unselectedLabelTextStyle:
+                  TextStyle(color: Theme.of(context).colorScheme.onSecondary),
+              selectedLabelTextStyle: TextStyle(
+                  color: Theme.of(context).colorScheme.onSecondary,
+                  fontWeight: FontWeight.bold),
+              onDestinationSelected: (int index) {
+                setState(() {
+                  _selectedSubmenuIndex = index;
+                });
+              },
+              extended: constraints.maxWidth >= 600,
+              destinations: const [
+                NavigationRailDestination(
+                  icon: Icon(Icons.search),
+                  selectedIcon: Icon(Icons.search),
+                  label: Text('Search'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.view_in_ar_sharp),
+                  selectedIcon: Icon(Icons.view_in_ar_sharp),
+                  label: Text('Inventory'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.cached_outlined),
+                  selectedIcon: Icon(Icons.cached_outlined),
+                  label: Text('Staples'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.one_x_mobiledata_outlined),
+                  selectedIcon: Icon(Icons.one_x_mobiledata_outlined),
+                  label: Text('One timers'),
+                ),
+              ],
+            )),
+            body,
+          ],
+        ),
       ),
     );
   }
