e289bd84fab1bf6df468dd04b297ff22419c4095
cryptoblivious <andrzej.wis123@gmail.com>
2023-06-06 09:56:20 -0400

    Added integrations for google sign-in

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 
 import 'pantry_page.dart';
-import 'pantry_item_card.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
