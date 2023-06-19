f525f94b0faf0ca3c8c6e42fa74f2c7a48644295
cryptoblivious <andrzej.wis123@gmail.com>
2023-06-19 17:15:19 -0400

    Removed leftover ChatGPT code in snackbar

--- lib/widgets/snackbars/generic_snackbar.dart
+++ lib/widgets/snackbars/generic_snackbar.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 void showGenericSnackbar(BuildContext context, {String? message}) {
   final String snackMessage = message ?? 'Generic Snackbar Message!';
 
-  final RenderBox? overlay = Overlay.of(context).context.findRenderObject() as RenderBox?;
-  final offset = overlay?.globalToLocal(Offset.zero);
-
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
       content: Center(
